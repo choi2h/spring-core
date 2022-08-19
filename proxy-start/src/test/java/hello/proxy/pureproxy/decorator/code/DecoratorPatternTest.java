39b127769b88109105fa5aea3c7ae0ad412aa605
ehchoi <chldlghk@gmail.com>
2022-08-19 13:18:51 +0900

    [ADD] Decorator Pattern 예제

---
+++ proxy-start/src/test/java/hello/proxy/pureproxy/decorator/code/DecoratorPatternTest.java
@@ -0,0 +1,25 @@
+package hello.proxy.pureproxy.decorator.code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DecoratorPatternTest {
+
+    @Test
+    void noDecorator() {
+        Component component = new RealComponent();
+        DecoratorPatternClient decoratorPatternClient = new DecoratorPatternClient(component);
+
+        decoratorPatternClient.execute();
+    }
+
+    @Test
+    void decorator1() {
+        Component component = new RealComponent();
+        MessageDecorator decorator = new MessageDecorator(component);
+        DecoratorPatternClient decoratorPatternClient = new DecoratorPatternClient(decorator);
+
+        decoratorPatternClient.execute();
+    }
+}
