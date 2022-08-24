537141cc3d74f4a76d20fe207a88ba98b6dfbd46
choiihwa <chldlghk@gmail.com>
2022-08-24 22:04:46 +0900

    ProxyFactory 적용-2

--- proxy-start/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy-start/src/main/java/hello/proxy/ProxyApplication.java
@@ -3,6 +3,7 @@ package hello.proxy;
 import hello.proxy.config.V2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.V2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -10,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(ProxyFactoryConfigV1.class) //클래스를 스프링 빈으로 등록
+@Import(ProxyFactoryConfigV2.class) //클래스를 스프링 빈으로 등록
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
